026e1b4f8b0dc23a9e3f898578033fec984f74a9
Caleb Stein <caleb@calebstein.net>
2023-05-04 16:27:40 -0700

    removed tests, we'll be live soon!

--- sidebar.js
+++ sidebar.js
@@ -1,8 +1,5 @@
 function generateSidebar() {
     document.getElementById("sidebar-links").innerHTML = `
         <li><a href="/">Courses</a></li>
-        <li><a href="/">Test 1</a></li>
-        <li><a href="/">Test 2</a></li>
-        <li><a href="/">Test 3</a></li>
     `;
 }
