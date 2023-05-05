b96d32280170fa5e44f6a364cf67618723452e8b
Caleb Stein <caleb@calebstein.net>
2023-05-04 21:24:51 -0700

    moved creation of course cards to its own script

---
+++ create-cards.js
@@ -0,0 +1,8 @@
+document.getElementById("artlist").innerHTML = `
+    <div id="environmental=toxicology">
+        <a href="courses/environmental-toxicology.html">
+            <img class="course-image" src="images/envtox.jpg" alt="course image">
+            <p>Environmental Toxicology</p>
+        </a>
+    </div>
+`
