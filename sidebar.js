dccf49f8c4863cf133d2285982238d175bcab639
Caleb Stein <caleb@calebstein.net>
2023-05-05 14:36:24 -0700

    Added new homepage

--- sidebar.js
+++ sidebar.js
@@ -1,5 +1,5 @@
 document.getElementById("sidebar-links").innerHTML = `
-    <li class="sidebar-link"><a href="/">Courses</a></li>
+    <li class="sidebar-link"><a href="/courses.html">Courses</a></li>
     <li class="sidebar-link"><a href="/support.html">Support</a></li>
     <li class="sidebar-link"><a href="/about.html">About</a></li>
 `;
