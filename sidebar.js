5209cb570ed4b2a615872485097da43c4aabcdec
Caleb Stein <caleb@calebstein.net>
2023-05-07 15:12:55 -0700

    added live courses link

--- sidebar.js
+++ sidebar.js
@@ -1,5 +1,6 @@
 document.getElementById("sidebar-links").innerHTML = `
-    <li class="sidebar-link"><a href="/courses.html">Courses</a></li>
+    <li class="sidebar-link"><a target="_blank" href="https://ipak-edu.org/registration/">Live<br>Courses</a></li>
+    <li class="sidebar-link"><a href="/courses.html">On-Demand<br>Courses</a></li>
     <li class="sidebar-link"><a href="/support.html">Support</a></li>
     <li class="sidebar-link"><a href="/about.html">About</a></li>
 `;
