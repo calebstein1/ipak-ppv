eea530280029d7408ee35370ed612173abd2ade0
Caleb Stein <caleb@calebstein.net>
2023-05-09 14:00:34 -0700

    moved sidebar footer section into sidebar.js

--- sidebar.js
+++ sidebar.js
@@ -1,7 +1,15 @@
-document.getElementById("sidebar-links").innerHTML = `
-    <li class="sidebar-link"><a target="_blank" href="https://ipak-edu.org/registration/">Live<br>Courses</a></li>
-    <li class="sidebar-link"><a href="/courses.html">On-Demand<br>Courses</a></li>
-    <li class="sidebar-link"><a href="/newsletter.html">Newsletter</a></li>
-    <li class="sidebar-link"><a href="/support.html">Support</a></li>
-    <li class="sidebar-link"><a href="/about.html">About</a></li>
+document.querySelector(".sidebar").innerHTML = `
+    <ul>
+        <li class="sidebar-link"><a target="_blank" href="https://ipak-edu.org/registration/">Live<br>Courses</a></li>
+        <li class="sidebar-link"><a href="/courses.html">On-Demand<br>Courses</a></li>
+        <li class="sidebar-link"><a href="/newsletter.html">Newsletter</a></li>
+        <li class="sidebar-link"><a href="/support.html">Support</a></li>
+        <li class="sidebar-link"><a href="/about.html">About</a></li>
+    </ul>
+    <div class="sidebar-btm">
+        <a class="sidebar-link" target="_blank" href="https://ipak-edu.org/">ipak-edu.org</a>
+            <p>© 2023 IPAK EDU</p>
+            <a href="/tos.html">Terms of Use</a>
+            <p>Site design by <a class="personal-link" target="_blank" href="https://calebstein.net">Caleb Stein</a></p>
+    </div>
 `;
