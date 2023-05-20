5519d100628b3216032bd860bb5a6b0b358bedff
Caleb Stein <caleb@calebstein.net>
2023-05-20 15:04:27 -0700

    rewrote loop to generate course links properly

--- create-cards.js
+++ create-cards.js
@@ -13,14 +13,26 @@ const courses = [
         "Environmental Toxicology"),
 ]
 
-courses.forEach(course => {
-    cardArea.innerHTML += `
-        <div id="${course.id}">
-            <a href="courses/${course.id}.html">
-                <img class="course-image" src="images/${course.img}" alt="course image">
-                <span class="course-desc">${course.desc}</span>
-                <p>${course.title}</p>
-            </a>
-        </div>
-    `
+courses.reverse().forEach(course => {
+    const cont = document.createElement("div");
+    const link = document.createElement("a");
+    const img = document.createElement("img");
+    const desc = document.createElement("span");
+    const title = document.createElement("p");
+
+    cont.setAttribute("id", `${course.id}`);
+    link.setAttribute("href", `courses/${course.id}.html`);
+    ( (el, attr) => {
+        for (let key in attr) el.setAttribute(key, attr[key]);
+    })(img, {"class": "course-image", "src": `images/${course.img}`, "alt": "course image",});
+    desc.setAttribute("class", "course-desc");
+
+    desc.innerText = `${course.desc}`;
+    title.innerText = `${course.title}`;
+
+    link.appendChild(img);
+    link.appendChild(desc);
+    link.appendChild(title);
+    cont.appendChild(link);
+    cardArea.appendChild(cont);
 });
