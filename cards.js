54bb0049a9d6233b015fbd8e51a223a040035afd
Caleb Stein <caleb@calebstein.net>
2023-06-10 15:05:37 -0700

    added vaccine course

--- cards.js
+++ cards.js
@@ -5,6 +5,13 @@ const courses = [
         "Environmental Toxicology",
         "1458452",
         "338a6ijjfhef404"),
+
+    new Card("vaccine-course",
+        "vaccine-course.svg",
+        "Students will learn via readings of primary literature facts about the history of the impact of vaccines on human health. This course will provide a solid basis of facts and realities of vaccines. Students will be able to use this knowledge in public discourse on vaccines in their personal lives and in interactions with others in the public square.",
+        "Vaccine Course",
+        "1495799",
+        "6404faegiaj4888"),
 ]
 
 function Card(id, img, desc, title, playlistId, selPlaylist) {
